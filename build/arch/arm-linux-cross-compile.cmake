0bd067a8a3003420f7ebe9783d9edabc75a335e4
chumoath <473358364@qq.com>
2023-07-18 01:21:34 +0800

    添加注释

--- build/arch/arm-linux-cross-compile.cmake
+++ build/arch/arm-linux-cross-compile.cmake
@@ -5,6 +5,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set (SDK_PATH /home/wjxh/linux/tool/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf)
 
+# 已经修改了 /etc/environment，不需要手动修改 PATH；可能和 sshd 启动的选项有关 /etc/profile
 #set(ENV{PATH} "$ENV{PATH}:${SDK_PATH}/bin")
 message("PATH: $ENV{PATH}")
 message("PWD: $ENV{PWD}")
@@ -12,12 +13,13 @@ message("PWD: $ENV{PWD}")
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
+# FIND_ROOT 包含了 LINUX 目录
 set (CMAKE_FIND_ROOT_PATH ${SDK_PATH}/arm-linux-gnueabihf ${LINUX_PATH})
 
-set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
-set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 
-set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 include_directories(${LINUX_PATH}/include)
