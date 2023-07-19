2f9f729980b7d1ba595050ed86f14c3883413dba
chumoath <473358364@qq.com>
2023-07-19 22:18:13 +0800

    添加 *.h 文件可以被 clion 提示的功能，只要被包含就可以了

---
+++ infected/modules/common/common.c
@@ -0,0 +1,11 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/export.h>
+
+// 头文件要想提示，就要被 include
+#include "common.h"
+
+int common_init_dev()
+
+EXPORT_SYMBOL()
---
+++ infected/modules/common/common.h
@@ -0,0 +1,10 @@
+#ifndef DRIVERS_COMMON_H
+#define DRIVERS_COMMON_H
+
+#include <linux/device.h>
+
+struct CDEV {
+
+};
+
+#endif //DRIVERS_COMMON_H
