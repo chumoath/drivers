ff5ff6394b445b9bb986095cbf4e83b41cfb1abe
chumoath <473358364@qq.com>
2023-07-30 11:04:43 +0800

    添加常用头文件

--- build/cmake/imx6ull.cmake
+++ build/cmake/imx6ull.cmake
@@ -1,4 +1,5 @@
-set (ko_target led proc i2c common)
+set (ko_target led proc i2c common inc)
 add_dependencies(led common)
 add_dependencies(proc common)
 add_dependencies(i2c common)
+add_dependencies(inc common)
--- build/cmake/linux_defines.cmake
+++ build/cmake/linux_defines.cmake
@@ -1,3 +1,4 @@
 add_definitions(-D__KERNEL__)
 add_definitions(-DMODULE)
 add_definitions(-DCONFIG_MODULES)
+add_definitions(-DCONFIG_MMU)
